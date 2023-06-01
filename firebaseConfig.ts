62fc1612617a5ac188d1c9623af051638465201c
jhlee <jhlee@spicusinc.com>
2023-06-01 18:57:59 +0900

    init service

---
+++ firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+}
+
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
