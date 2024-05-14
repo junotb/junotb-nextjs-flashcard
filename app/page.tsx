d9dcc15963b82aa25ee7dccf491b062f3782be5c
jhlee <jhlee@spicusinc.com>
2024-05-14 20:45:49 +0900

    edit service

--- app/page.tsx
+++ app/page.tsx
@@ -27,36 +27,38 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className='flex flex-col w-full h-full'>
       <div className="flex justify-start items-center border-b px-4 w-full h-20 bg-neutral-800">
         <h1 className="text-2xl font-bold">Flashcard</h1>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto p-6 gap-6">
-        {
-          words.map((word) => 
-            <div
-              key={word.id}
-              className="flex flex-col gap-2 border p-4 w-60 h-[20rem] bg-black hover:bg-neutral-800 rounded-lg"
-            >
-              <h3 className="border-b pb-2 text-lg font-semibold">{word.korean_name}&nbsp;({word.english_name})</h3>
-              <h4>{word.description}</h4>
-            </div>
-          )
-        }
-        <div
-          onClick={handleIsActive}
-          className="flex justify-center items-center border w-60 h-[20rem] bg-black hover:bg-neutral-800 rounded-lg"
-        >
-          <span className="text-6xl" aria-hidden="true">+</span>
+      <div className="flex flex-col w-full h-full overflow-y-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-auto p-6 gap-6">
+          {
+            words.map((word) => 
+              <div
+                key={word.id}
+                className="flex flex-col gap-2 border p-4 w-60 h-[20rem] bg-black hover:bg-neutral-800 rounded-lg"
+              >
+                <h3 className="border-b pb-2 text-lg font-semibold">{word.korean_name}&nbsp;({word.english_name})</h3>
+                <h4>{word.description}</h4>
+              </div>
+            )
+          }
+          <div
+            onClick={handleIsActive}
+            className="flex justify-center items-center border w-60 h-[20rem] bg-black hover:bg-neutral-800 rounded-lg"
+          >
+            <span className="text-6xl" aria-hidden="true">+</span>
+          </div>
         </div>
+        {
+          isActive &&
+          <Modal
+            handleIsActive={handleIsActive}
+            handleGetWords={handleGetWords}
+          />
+        }
       </div>
-      {
-        isActive &&
-        <Modal
-          handleIsActive={handleIsActive}
-          handleGetWords={handleGetWords}
-        />
-      }
     </div>
   )
 };
